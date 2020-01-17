7bc540182bb647e071ad2a7c7a1efd01898b0ab0
Patrick Duquette <duquetteman@gmail.com>
2020-01-16 21:01:52 -0600

    Work in progress.

--- index.js
+++ index.js
@@ -1,32 +1,79 @@
-const fs = require("fs");
 const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-let questions = [
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
     {
-        message: 'What is your github username?',
-        name: 'username',
+      type: "input",
+      name: "name",
+      message: "What is your name?"
     },
     {
-        message: 'What is your favorite color',
-        name: 'color',
-        type: 'list',
-        choices: ['green', 'blue', 'pink', 'red'],
+      type: "input",
+      name: "location",
+      message: "Where are you from?"
+    },
+    {
+      type: "input",
+      name: "hobby",
+      message: "What is your favorite hobby?"
+    },
+    {
+      type: "input",
+      name: "food",
+      message: "What is your favorite food?"
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username"
+    },
+    {
+      type: "input",
+      name: "linkedin",
+      message: "Enter your LinkedIn URL."
     }
-]
-console.log(questions)
+  ]);
+}
 
-// switch (color) {
-//     case 'green':
-//     break;
-//     case 'blue':
-//     break;
-//     case 'pink':
-//     break;
-//     case 'red':
-//     break;
-// }
-// console.log(color)
+function generateHTML(answers) {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+    <p class="lead">I am from ${answers.location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answers.github}</li>
+      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+}
 
+promptUser()
+  .then(function(answers) {
+    const html = generateHTML(answers);
 
-
-
+    return writeFileAsync("index.html", html);
+  })
+  .then(function() {
+    console.log("Successfully wrote to index.html");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
